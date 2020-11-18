acefeaeb602dd1d6a2f6bc06b94b715b9283bd23
Kha Nguyen <nlhkha@gmail.com>
2020-11-18 13:21:49 +0200

    add pylint rules, add current time

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from typing import Optional
+import time
 
 from fastapi import FastAPI
 
@@ -7,12 +8,16 @@ from hsl_services import get_routes
 
 from routers import hsl_v1
 
-app = FastAPI(title="HSL Timer API")
+app = FastAPI(
+    title="HSL Timer API",
+    description="This API searches for up coming nearby bus/tram routes, provided a location.",
+    version="1.0")
 
-@app.get("/")
+@app.get("/", tags=["health-check"], description="The time in the response is in UTC milliseconds.")
 def read_root():
     return {
-        'message': "Welcome to HSL Timer API"
+        'message': "Welcome to HSL Timer API",
+        'currentUTCTime': round(time.time() * 1000)
     }
 
 app.include_router(hsl_v1.router, prefix='/v1', tags=['hsl', 'v1'])
